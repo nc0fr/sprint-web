301e2b801f6af151678cad9920f92a85b5303e10
Lucas PAULO <lpadressepro@gmail.com>
2023-12-14 17:38:04 +0100

    Ajout fonctionnalité suppression création typecompte et typecontrat
    Signed-off-by: Lucas PAULO <lpadressepro@gmail.com>

--- mvc/Modele/modele.php
+++ mvc/Modele/modele.php
@@ -49,3 +49,116 @@ function mdlModifierPiece($id, $value){
     $resultat->setFetchMode(PDO::FETCH_OBJ);
     $resultat->fetchAll();
 }
+
+function mdlGetAllTypeAccount(){
+
+    $connexion = getConnexion();
+
+    $requete = "SELECT * FROM typecompte;";
+    $resultat = $connexion->query($requete);
+    $resultat->setFetchMode(PDO::FETCH_OBJ);
+    $typeAccount = $resultat->fetchAll();
+    $resultat->closeCursor();
+
+    return $typeAccount;
+}
+
+function mdlGetAllTypeContract(){
+
+    $connexion = getConnexion();
+
+    $requete = "SELECT * FROM typecontrat;";
+    $resultat = $connexion->query($requete);
+    $resultat->setFetchMode(PDO::FETCH_OBJ);
+    $typeContract = $resultat->fetchAll();
+    $resultat->closeCursor();
+
+    return $typeContract;
+}
+
+function mdlAjouterType($nature, $nom, $pieceCreation, $pieceModification, $pieceSuppression){
+    $connexion = getConnexion();
+
+    $requete = 'INSERT INTO type'.$nature.'(nom) VALUES("'.$nom.'");
+                INSERT INTO motif (libelle, justificatifs) VALUES
+                ("Création '."d'un ".$nom.'","'.$pieceCreation.'"),
+                ("Modification '."d'un ".$nom.'","'.$pieceModification.'"),
+                ("Suppression '."d'un ".$nom.'","'.$pieceSuppression.'");';
+    
+    $resultat = $connexion->query($requete);
+    $resultat->setFetchMode(PDO::FETCH_OBJ);
+    $resultat->fetch();
+    $resultat->closeCursor();
+}
+
+function  mdlTypeIsAssign($id, $type){
+
+    $connexion = getConnexion();
+
+    $requete = '';
+    if($type == 'account'){
+        $requete = 'SELECT * FROM estdetypecompte WHERE typeCompte = '.$id.';';
+    }elseif($type == 'contract'){
+        $requete = 'SELECT * FROM estdetypecontrat WHERE typeContrat = '.$id.';';
+    }else{
+        throw new Exception("Type non définie pour la requête TypeIsAssign");
+    }
+    $resultat = $connexion->query($requete);
+    $resultat->setFetchMode(PDO::FETCH_OBJ);
+    $verifIsAssign = $resultat->fetch();
+    $resultat->closeCursor();
+
+    return $verifIsAssign;
+}
+
+function mdlSupprimerType($id, $type){
+
+    $connexion = getConnexion();
+    
+    $requete = '';
+    if($type == 'account'){
+        $requete = 'DELETE FROM typecompte WHERE id = '.$id.';';
+    }elseif($type == 'contract'){
+        $requete = 'DELETE FROM typecontrat WHERE id = '.$id.';';
+    }else{
+        throw new Exception("Type non définie pour la requête SupprimerType");
+    }
+    $resultat = $connexion->query($requete);
+    $resultat->setFetchMode(PDO::FETCH_OBJ);
+    $resultat->fetch();
+    $resultat->closeCursor();
+}
+
+function mdlGetType($id, $type){
+
+    $connexion = getConnexion();
+
+    $requete = '';
+    if($type == 'account'){
+        $requete = 'SELECT * FROM typecompte WHERE id = '.$id.';';
+    }elseif($type == 'contract'){
+        $requete = 'SELECT * FROM typecontrat WHERE id = '.$id.';';
+    }else{
+        throw new Exception("Type non définie pour la requête GetType");
+    }
+
+    $resultat = $connexion->query($requete);
+    $resultat->setFetchMode(PDO::FETCH_OBJ);
+    $type = $resultat->fetch();
+    $resultat->closeCursor();
+
+    return $type;
+}
+
+function mdlSupprimerMotif($name){
+
+    $connexion = getConnexion();
+
+    $requete = 'DELETE FROM motif WHERE libelle = "Création '."d'un ".$name.
+                '" OR libelle = "Modification '."d'un ".$name.
+                '" OR libelle = "Suppression '."d'un ".$name.'";';
+    $resultat = $connexion->query($requete);
+    $resultat->setFetchMode(PDO::FETCH_OBJ);
+    $resultat->fetch();
+    $resultat->closeCursor();
+}
--- mvc/Vue/vue.php
+++ mvc/Vue/vue.php
@@ -69,6 +69,59 @@ function vueMsgDirecteur($msg){
     require_once('vue/gabaritDirecteur.php');
 }
 
+function vueGetAllTypeAccountContract($account, $contract){
+    $contenu = '<fieldset><legend>Liste des types de Compte</legend><form method="post" action="sprintBank.php">';
+    if(sizeof($account) > 0){
+        $contenu = $contenu.'<ul>';
+        foreach($account as $value){
+            $contenu = $contenu.'<td><input type="radio" name="account" value='.$value->id.'>'.$value->nom.'</td><br><br>';
+        }
+        $contenu = $contenu.'</ul><input type="submit" name="supprimerType" value="Supprimer le type de compte selectionné"/></form></fieldset>';
+    }
+    $contenu = $contenu.'<fieldset><legend>Liste des types de Contrat</legend><form method="post" action="sprintBank.php">';
+    if(sizeof($contract) > 0){
+        $contenu = $contenu.'<ul>';
+        foreach($contract as $value){
+            $contenu = $contenu.'<td><input type="radio" name="contract" value='.$value->id.'>'.$value->nom.'</td><br><br>';
+        }
+        $contenu = $contenu.'</ul><input type="submit" name="supprimerType" value="Supprimer le type de contrat selectionné"/></form></fieldset>';
+    }
+
+    $contenu = $contenu.'<form method="post" action="sprintBank.php">
+                            <fieldset>
+                                <legend>Creer un nouveau Type</legend>
+                                <p>
+                                    <label>Nature du changement :</label>
+                                    <select name="nature">
+                                        <option value="compte" selected>Compte</option>
+                                        <option value="contrat">Contrat</option>
+                                    </select>
+                                </p>
+                                <p>
+                                    <label>Nom :</label>
+                                    <input type="text" name="nom"/>
+                                </p>
+                                <p>
+                                    <label>Pièces justificative pour Création</label>
+                                    <input type="text" name="pieceCreation"/>
+                                </p>
+                                <p>
+                                    <label>Pièces justificative pour Modification</label>
+                                    <input type="text" name="pieceModification"/>
+                                </p>
+                                <p>
+                                    <label>Pièces justificative pour Suppression</label>
+                                    <input type="text" name="pieceSuppression"/>
+                                </p>
+                                <p>
+                                    <input type="submit" name="ajouterType" value="Ajouter le type"/>
+                                </p>
+                            </fieldset>
+                        </form>';
+
+    require_once('vue/gabaritDirecteur.php');
+}
+
 function afficherErreur($erreur){
     $contenu='<p>'. $erreur.'</p>';
     require_once('Vue/gabaritLogin.php');
