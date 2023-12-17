76adffd0337088e167b74540d26cefac06254e4b
Nicolas Paul <n@nc0.fr>
2023-12-17 17:16:13 +0100

    Execute Pint en mode de test pendant le CI
    From Nicolas Paul <n@NC0.fr>
    
    Le CI n'exécutait pas Pint en mode de test, ce qui faisait qu'il n'y
    avait pas de rapport sur le style.
    
    Signed-off-by: Nicolas Paul <n@NC0.fr>

--- mvc/sprintBank.php
+++ mvc/sprintBank.php
@@ -5,14 +5,11 @@ require_once 'Controleur/controleur.php';
 try {
     if (isset($_POST['connexion'])) {
         ctrlVerifierId();
-
     } elseif (isset($_GET['action1']) == 'gestion_motifs') {
         ctrlGetAllMotif();
-
     } elseif (isset($_POST['modifierPiece'])) {
 
         ctrlModifierPiece($_POST);
-
     } elseif (isset($_GET['action']) == 'gestion_employes') {
         if (isset($_POST['ajtemploye'])) {
             ctrlAjouterEmploye();
@@ -21,7 +18,6 @@ try {
         } else {
             ctrlGestion();
         }
-
     } elseif (isset($_GET['action2']) == 'gestion_clients') {
         if (isset($_POST['choixmodif'])) {
             ctrlModifierClient();
@@ -31,15 +27,12 @@ try {
     } elseif (isset($_GET['action3']) == 'gestion_comptes_contrats') {
 
         ctrlGetAllTypeAccountContract();
-
     } elseif (isset($_POST['supprimerType'])) {
 
         ctrlSupprimerTypeAccount($_POST);
-
     } elseif (isset($_POST['ajouterType'])) {
 
         ctrlAjouterType($_POST);
-
     } else {
         ctrlPageLogin();
     }
