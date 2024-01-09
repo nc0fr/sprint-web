fc5f13a8d45ddb2b3dccf7417ba15d577b87e499
Younes <ouaammouyounes@gmail.com>
2024-01-09 13:59:37 +0100

    Optimisation gabarits

--- mvc/Vue/gabaritAgent.php
+++ mvc/Vue/gabaritAgent.php
@@ -7,15 +7,30 @@
     </head>
     <body>
     <div class="navbar">
-      <div class="infos"><?php echo $contenu; ?></div>
+    <div class="infos">
+        <?php if (! empty($contenu1)) {
+            echo $contenu1;
+        } ?>
+    </div>
+
       <a href="?action=gestion_clients"><div class="item">Modifier client</div></a>
-      <a href="?action=synthese"><div class="item">Synthèse client</div></a>
+      <a href= "?action=synthese"><div class="item">Synthèse client</div></a>
       <a href="?action=operations"><div class="item">Effectuer opération</div></a>
       <div class="item">Gestion RDV</div>
       <a href="sprintBank.php"><div class="logout">Déconnexion</div></a>
     </div>
+    <form method="post" class="formulaire">
+        <?php
+        if (isset($contenu)) {
+            echo '<p>'.$contenu.'</p>';
+        }
+        ?>
+    </form>
+
+
+
+
 
 
     </body>
 </html>
-
